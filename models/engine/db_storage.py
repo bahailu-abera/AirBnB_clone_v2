52bae5929db025019f7ca46106cc9537251cdaf7
Bahailu Abera <aberabahailu4@gmail.com>
2023-04-09 15:27:22 +0300

    fixBug: import error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -34,6 +34,7 @@ class DBStorage:
         """
         Retrieves all object depending on cls name
         """
+        from models.base_model import Base
         if cls:
             objs = self.__session.query(cls).all()
         else:
