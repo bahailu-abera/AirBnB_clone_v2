13e1f25addbbd22611e3b97b2bb58f72e3150a55
Bahailu Abera <aberabahailu4@gmail.com>
2023-04-09 19:44:56 +0300

    remove unecessary commentes

--- models/state.py
+++ models/state.py
@@ -1,32 +1,3 @@
-# #!/usr/bin/python3
-# """ State Module for HBNB project """
-# from models.base_model import BaseModel, Base
-# from sqlalchemy import Column, Integer, String, ForeignKey
-# from sqlalchemy.orm import relationship
-# from models import storage
-# from models.city import City
-# from os import environ
-
-
-# class State(BaseModel, Base):
-#     """ The city class, contains state ID and name """
-#     __tablename__ = "states"
-#     name = Column(String(128), nullable=False)
-
-#     if (environ.get("HBNB_TYPE_STORAGE") == "db"):
-#         cities = relationship("City", backref="state", cascade="all, delete")
-#     else:
-#         @property
-#         def cities(self):
-#             """ getter method for cities"""
-#             cities_dict = []
-#             city_dict = storage.all(City)
-#             if city_dict:
-#                 for key in city_dict:
-#                     if self.id == city_dict[key].state_id:
-#                         cities_dict.append(city_dict[key])
-#             return cities_dict
-
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
