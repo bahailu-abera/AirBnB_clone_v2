c3f69bb264528bb629c83da96669b8b549a5532d
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-29 11:21:01 +0300

    correct folder permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 # Install Nginx if not already installed
 
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+sudo apt-get update
+sudo apt-get -y install nginx
 
 # Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file for testing
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
-<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-EOF
+</html>" > /data/web_static/releases/test/index.html
 
 # Create or recreate symbolic link
 sudo rm -rf /data/web_static/current
@@ -30,7 +26,7 @@ sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 config_file="/etc/nginx/sites-available/default"
-sudo sed -i '/^\tlocation \/ {/a \\\t\talias /data/web_static/current/;' $config_file
+sed -i '/^\tlocation \/ {/a \\\t\talias /data/web_static/current/;' $config_file
 
 # Restart Nginx
 sudo service nginx restart
