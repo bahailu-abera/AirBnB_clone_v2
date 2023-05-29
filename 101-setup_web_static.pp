efb4345ad838dc800e30f91892228de2e401e86d
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-29 16:14:59 +0300

    setup web server using puppet

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,66 @@
+file { '/data':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'file',
+  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "
+server {
+    listen 80;
+    listen [::]:80 default_server;
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+}
+",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
