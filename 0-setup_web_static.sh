f981f841e9e81d312383f1e49c7a129c314505ca
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-29 10:59:05 +0300

    the second line must be a comment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-
 # Install Nginx if not already installed
+
 if ! command -v nginx &> /dev/null; then
-    apt-get update
-    apt-get -y install nginx
+    sudo apt-get update
+    sudo apt-get -y install nginx
 fi
 
 # Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file for testing
-echo "<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
@@ -20,17 +20,17 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 
 # Create or recreate symbolic link
-rm -rf /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Set ownership of /data/ folder to ubuntu user and group recursively
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 config_file="/etc/nginx/sites-available/default"
-sed -i '/^\tlocation \/ {/a \\\t\talias /data/web_static/current/;' $config_file
+sudo sed -i '/^\tlocation \/ {/a \\\t\talias /data/web_static/current/;' $config_file
 
 # Restart Nginx
-service nginx restart
+sudo service nginx restart
 
 exit 0
