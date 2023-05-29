019f3cb6a93e5e8847e3a4b9a2f2a04c44991d9e
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-29 11:39:01 +0300

    change the folder permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
-
-sudo apt-get update
+#this script configures nginx in a new server
+sudo apt-get -y update
 sudo apt-get -y install nginx
-
-# Create necessary directories if they don't exist
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file for testing
 echo "<html>
   <head>
   </head>
@@ -16,19 +11,7 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-
-# Create or recreate symbolic link
-sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership of /data/ folder to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-sed -i '/^\tlocation \/ {/a \\\t\talias /data/web_static/current/;' $config_file
-
-# Restart Nginx
+sed -i '/listen 80 default_server;/a location /static/ { alias /data/web_static/current/; autoindex off;}' /etc/nginx/sites-available/default
 sudo service nginx restart
-
-exit 0
