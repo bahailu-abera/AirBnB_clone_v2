68c4d2a789c14e5ca67b5bdecd195c1c98331a9c
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-29 13:24:03 +0300

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
 #this script configures nginx in a new server
-sudo apt-get -y update
-sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<html>
+
+# Install Nginx if not already installed
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file for testing
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create or recreate symbolic link
+sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Set ownership of /data/ folder to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server;/a location /static/ { alias /data/web_static/current/; autoindex off;}' /etc/nginx/sites-available/default
+
+# Update Nginx configuration
+config_file="/etc/nginx/sites-available/default"
+sudo sed -i '/^\tlocation \/ {/a alias /data/web_static/current/;' $config_file
+
+# Restart Nginx
 sudo service nginx restart
+
+exit 0
