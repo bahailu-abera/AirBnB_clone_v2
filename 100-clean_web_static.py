d41187a17530e1735e7d4f75cd1b40263a019492
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-29 16:03:07 +0300

    clean unnecessary files

---
+++ 100-clean_web_static.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""
+Fabric script to generate a .tgz archive from the contents of web_static folder
+"""
+
+import os
+from fabric.api import env, run, put, local
+from datetime import datetime
+
+
+# Web servers IP addresses
+env.hosts = ['54.157.167.29', '18.235.255.184']
+# SSH user
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder"""
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(current_time)
+
+    local("mkdir -p versions")
+    result = local("tar -czvf {} web_static".format(archive_path))
+
+    if result.succeeded:
+        return archive_path
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to web servers
+    """
+    if not archive_path:
+        return False
+
+    try:
+        archive_name = archive_path.split('/')[-1]
+        archive_no_ext = archive_name.split('.')[0]
+
+        # Upload the archive to /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+
+        # Uncompress the archive to /data/web_static/releases/<archive_no_ext>/
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_no_ext))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(archive_name, archive_no_ext))
+
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(archive_name))
+
+        # Move the uncompressed files to the proper location
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'
+            .format(archive_no_ext, archive_no_ext))
+
+        # Remove the unnecessary directory
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_no_ext))
+
+        # Delete the existing symbolic link
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_no_ext))
+
+        print("New version deployed!")
+        return True
+
+    except Exception:
+        return False
+
+
+def deploy():
+    """
+    Deploys the web server
+    """
+    path = do_pack()
+
+    if not path:
+        return False
+
+    return do_deploy(path)
+
+
+def do_clean(number=0):
+    """
+    Deletes out-of-date archives
+    """
+    number = int(number)
+    if number < 1:
+        number = 1
+    else:
+        number += 1
+
+    with lcd("versions"):
+        local("ls -1t | tail -n +{} | xargs -I {{}} rm -- {{}}"
+              .format(number))
+
+    with cd("/data/web_static/releases"):
+        run("ls -1t | tail -n +{} | xargs -I {{}} rm -rf -- {{}}"
+            .format(number))
+
+        run("ls -1t /data/web_static/releases | tail -n +{}"
+            .format(number))
