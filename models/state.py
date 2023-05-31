806aff34c069195d2e0623b412cbb44405668f59
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-31 13:08:43 +0300

    add getter method for cities

--- models/state.py
+++ models/state.py
@@ -4,18 +4,23 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+from os import environ
+
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
 
-    @property
-    def cities(self):
-        from models import storage
-        city_list = []
-        for city in storage.all('City').values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete')
+
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            city_list = []
+            for city in storage.all('City').values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
