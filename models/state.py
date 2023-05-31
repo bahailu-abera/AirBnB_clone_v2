6aebd19ffb2c16a5e72d24825071ab4fd77dd41a
Bahailu Abera <aberabahailu4@gmail.com>
2023-05-31 13:25:44 +0300

    add getter for cities

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -20,7 +21,9 @@ class State(BaseModel, Base):
         def cities(self):
             from models import storage
             city_list = []
-            for city in storage.all('City').values():
+            all_cities = storage.all(City)
+
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
